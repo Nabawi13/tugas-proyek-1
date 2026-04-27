3f005a9ae80a46e38675b3d51422b741c107ae0f
Rafid Nabawi <rafidnabawi.com>
2026-04-27 14:32:00 +0700

    update project laravel

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/kontak', [HomeController::class, 'kontak']);
+Route::get('/proofil', [HomeController::class, 'profil']);
